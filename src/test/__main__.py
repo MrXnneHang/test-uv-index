41c54a4f0e22839b64a92829cd0d47beba2ae57e
MrXnneHang <XnneHang@gmail.com>
2025-05-15 15:52:34 +0800

    :recycle: refactor: print insead of savefig.

--- src/test/__main__.py
+++ src/test/__main__.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy
+import torch
 
 
 def main():
     print(f"matplotlib:{matplotlib.__version__}")
     print(f"numpy:{numpy.__version__}")
+    print(f"torch:{torch.__version__}")
+    print(f"torch cuda available:{torch.cuda.is_available()}")
 
     x = numpy.linspace(0, 2 * numpy.pi, 100)
     y = numpy.sin(x)
-    plt.plot(x, y)
-    plt.savefig("sine_wave.png")  # 将图形保存到文件
+    print(torch.tensor(y))
